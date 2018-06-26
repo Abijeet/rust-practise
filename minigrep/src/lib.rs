f1ca5472c0a8b331b84ffb83567f4d7a0630c14d
Abijeet <abijeetpatro@gmail.com>
2018-06-26 08:12:56 +0530

    Minigrep: Implemented the search functionality.
    Signed-off-by: Abijeet <abijeetpatro@gmail.com>

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -22,11 +22,58 @@ impl Config {
 }
 
 pub fn run(config: Config) -> Result<(), Box<Error>> {
-    let mut f = File::open(config.filename)?;
+    let mut f = File::open(&config.filename)?;
     let mut content = String::new();
 
     f.read_to_string(&mut content)?;
+    let results = search(&content, &config.query);
 
-    println!("File contents: {}", content);
+    if results.len() == 0 {
+        println!("Search string {} not found in file - {}.", config.query, config.filename);
+        return Ok(());
+    }
+
+    for result in results {
+        println!("#{}: {}", result.line_num, result.line);
+    }
     Ok(())
+}
+
+pub struct SearchResult <'a> {
+    pub line_num: i32,
+    pub line: &'a str
+}
+
+pub fn search<'a>(content: &'a str, query: &str) -> Vec<SearchResult <'a>> {
+    let mut line_num = 1;
+    let mut results = Vec::new();
+    for line in content.lines() {
+        if line.contains(query) {
+            results.push(SearchResult {
+                line_num: line_num,
+                line: line
+            })
+        }
+        line_num += 1;
+    }
+    results
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn does_not_match() {
+        let content = "\
+            Rust is a 
+            static, concurrent, parallel,
+            programming language.
+            That kicks ass!!
+        ";
+        let query = "Rust";
+        let search_results: Vec<SearchResult> = search(content, query);
+        assert_eq!(search_results.len(), 1);
+        assert_eq!(search_results[0].line_num, 1);
+    }
 }
