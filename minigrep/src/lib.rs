561d680eccc9af0d07a1b791e2eb3a01d6d0b0e0
Abijeet <abijeetpatro@gmail.com>
2018-06-28 23:26:29 +0530

    Added case insensitive search and related tests.
    Signed-off-by: Abijeet <abijeetpatro@gmail.com>

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,10 +1,12 @@
 use std::fs::File;
 use std::io::prelude::*;
 use std::error::Error;
+use std::env;
 
 pub struct Config {
     pub query: String,
-    pub filename: String
+    pub filename: String,
+    pub case_sensitive: bool
 }
 
 impl Config {
@@ -14,9 +16,12 @@ impl Config {
         }
         let filename = args[1].clone();
         let query = args[2].clone();
-        Ok(Config { 
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config {
             filename,
-            query
+            query,
+            case_sensitive
         })
     }
 }
@@ -26,7 +31,12 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
     let mut content = String::new();
 
     f.read_to_string(&mut content)?;
-    let results = search(&content, &config.query);
+
+    let results = if config.case_sensitive {
+        search(&content, &config.query)
+    } else {
+        search_case_insensitive(&content, &config.query)
+    };
 
     if results.len() == 0 {
         println!("Search string {} not found in file - {}.", config.query, config.filename);
@@ -48,11 +58,27 @@ pub fn search<'a>(content: &'a str, query: &str) -> Vec<SearchResult <'a>> {
     let mut line_num = 1;
     let mut results = Vec::new();
     for line in content.lines() {
-        if line.contains(query) {
+        if line.contains(&query) {
             results.push(SearchResult {
                 line_num: line_num,
                 line: line
-            })
+            });
+        }
+        line_num += 1;
+    }
+    results
+}
+
+pub fn search_case_insensitive<'a>(content: &'a str, query: &str) -> Vec<SearchResult <'a>> {
+    let mut line_num = 1;
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+    for line in content.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(SearchResult {
+                line_num: line_num,
+                line: line
+            });
         }
         line_num += 1;
     }
@@ -64,15 +90,57 @@ mod test {
     use super::*;
 
     #[test]
-    fn does_not_match() {
+    fn does_match() {
         let content = "\
-            Rust is a 
+            Rust is a
             static, concurrent, parallel,
             programming language.
             That kicks ass!!
         ";
         let query = "Rust";
         let search_results: Vec<SearchResult> = search(content, query);
+        assert_eq!(search_results.len(), 1);
+        assert_eq!(search_results[0].line_num, 1);
+    }
+
+    #[test]
+    fn does_not_match() {
+        let content = "\
+            Rust is a
+            static, concurrent, parallel,
+            programming language.
+            That kicks ass!!
+        ";
+        let query = "poling";
+        let search_results: Vec<SearchResult> = search(content, query);
+        assert_eq!(search_results.len(), 0);
+    }
+
+    #[test]
+    fn multiple_matches() {
+        let content = "\
+            Rust is a
+            static, concurrent, parallel,
+            programming language.
+            Rust kicks ass!!
+        ";
+        let query = "Rust";
+        let search_results: Vec<SearchResult> = search(content, query);
+        assert_eq!(search_results.len(), 2);
+        assert_eq!(search_results[0].line_num, 1);
+        assert_eq!(search_results[1].line_num, 4);
+    }
+
+    #[test]
+    fn does_match_case_insensitive() {
+        let content = "\
+            Rust is a
+            static, concurrent, parallel,
+            programming language.
+            That kicks ass!!
+        ";
+        let query = "rust";
+        let search_results: Vec<SearchResult> = search_case_insensitive(content, query);
         assert_eq!(search_results.len(), 1);
         assert_eq!(search_results[0].line_num, 1);
     }
