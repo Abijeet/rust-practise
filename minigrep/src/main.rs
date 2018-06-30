ce2b3f73f69a1aae4b21321ae21ad17d8a273ed5
Abijeet <abijeetpatro@gmail.com>
2018-06-30 21:58:11 +0530

    Printing errors to the std error.
    Signed-off-by: Abijeet <abijeetpatro@gmail.com>

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,10 +4,10 @@ use std::env;
 use std::process;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();    
+    let args: Vec<String> = env::args().collect();
 
     let config = minigrep::Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem while parsing the arguments: {}", err);
+        eprintln!("Problem while parsing the arguments: {}", err);
         process::exit(1);
     });
 
@@ -15,7 +15,7 @@ fn main() {
     println!("In file: {}", config.filename);
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
